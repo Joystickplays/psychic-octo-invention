87325ff3db2bc0ddbf9dcd52b5f48512af48889f
Joystick <joystickgaming09@gmail.com>
2024-05-12 15:07:35 +0700

    Create testModule.lua

---
+++ testModule.lua
@@ -0,0 +1,21 @@
+local module = {}
+
+
+
+
+module["Name"] = "Custom module test"
+
+module[1] = {
+	Type = "Text",
+	Args = {"Hello!"}
+}
+
+module[2] = {
+	Type = "Button",
+	Args = {"Say hi", function(Self)
+		print("Hi!")
+	end,}
+}
+
+
+_G.Modules[#_G.Modules] = module
