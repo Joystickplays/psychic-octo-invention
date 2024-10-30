bc8b5c60e4d1017878e549b7116d830c2222d78e
Joystick <joystickgaming09@gmail.com>
2024-10-30 19:03:25 +0700

    Update yarhm.lua

--- yarhm.lua
+++ yarhm.lua
@@ -1,6 +1,4 @@
--- Now script requests are directly get from website
-loadstring(game:HttpGet("https://yarhm.goteamst.com/scr", false))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
 
-
--- TEMPORARY LOAD FROM GITHUB
---loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
+-- YARHM now loads with GitHub, and might be permanently. To load from the official website:
+-- loadstring(game:HttpGet("https://yarhm.goteamst.com/scr", false))()
