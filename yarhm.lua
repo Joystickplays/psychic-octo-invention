117a2ee7c33c6e2e887a6842fdc770cbe8a875bd
Joystick <joystickgaming09@gmail.com>
2025-03-10 07:09:19 +0700

    yes!

--- yarhm.lua
+++ yarhm.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://yarhm.mhi.im/scr", false))() -- thanks mochig for domain
+loadstring(game:HttpGet("https://yarhm.mhi.im/scr", false))() -- thanks mochig for domain & hosting
 
 -- Optional loading from GitHub if the website is down
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
