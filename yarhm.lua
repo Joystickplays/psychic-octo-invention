470ca86e61353f8a91eb47f6259ba83923f462bb
Joystick <joystickgaming09@gmail.com>
2025-06-23 01:36:27 +0700

    idk

--- yarhm.lua
+++ yarhm.lua
@@ -1,4 +1,19 @@
-loadstring(game:HttpGet("https://yarhm.mhi.im/scr", false))() -- thanks mochig for domain & hosting
+local src = ""
+local CoreGui = game:GetService("StarterGui")
+
+pcall(function() 
+    src = game:HttpGet("https://yarhm.mhi.im/scr", false) -- thanks mochig for domain & hosting
+end)
+if src == "" then
+  CoreGui:SetCore("SendNotification", {
+  	Title = "YARHM Outage";
+  	Text = "YARHM Online is currently unavailable! Sorry for the inconvenience. Using YARHM Offline.";
+	  Duration = 5;
+  })
+  src = game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua", false)
+
+
+loadstring(src)() 
 
 -- Optional loading from GitHub if the website is down
 --loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.18/yarhm.lua"))()
