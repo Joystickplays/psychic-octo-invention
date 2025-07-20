5927d6db45ccf30185b5b591210c5a681aa47b1d
Joystick <joystickgaming09@gmail.com>
2025-07-20 14:21:56 +0700

    Update yarhm.lua

--- yarhm.lua
+++ yarhm.lua
@@ -2,19 +2,16 @@ local src = ""
 local CoreGui = game:GetService("StarterGui")
 
 pcall(function() 
-    src = game:HttpGet("https://yarhm.mhi.im/scr", false) -- thanks mochig for domain & hosting
+    src = game:HttpGet("https://rawscripts.net/raw/Universal-Script-YARHM-12403", false)
 end)
 if src == "" then
   CoreGui:SetCore("SendNotification", {
   	Title = "YARHM Outage";
-  	Text = "YARHM Online is currently unavailable! Sorry for the inconvenience. Using YARHM Offline.";
+  	Text = "Scriptblox is currently unavailable! Sorry for the inconvenience. Using YARHM Offline.";
 	  Duration = 5;
   })
   src = game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.19/yarhm.lua", false)
 end
 
 
-loadstring(src)() 
-
--- Optional loading from GitHub if the website is down
---loadstring(game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.19/yarhm.lua"))()
+loadstring(src)()
